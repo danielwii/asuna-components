bb8d9d660e55350a7d413cdcfd1f7e90fe261026
Daniel Wei <danielwii0326@gmail.com>
2020-09-24 21:19:11 +0800

    refactor: components

--- stories/preview.stories.tsx
+++ stories/preview.stories.tsx
@@ -4,16 +4,20 @@ import React from 'react';
 
 import { PdfButton, Preview } from '../src';
 
-storiesOf('Preview', module).add('default', () => (
-  <div style={{ margin: '1rem' }}>
-    <PdfButton pdf="1.pdf" name="name.pdf" />
-    <hr />
-    <PdfButton pdf="2.pdf" />
-    <hr />
-    <PdfButton pdf="1.pdf" name="name.pdf" title="name.pdf" />
-    <hr />
-    <Preview text={JSON.stringify({ a: 1, b: new Date(), c: '123' })} jsonMode />
-    <hr />
-    <Preview text={JSON.stringify({ a: 1, b: new Date(), c: '234' })} listMode />
-  </div>
-));
+storiesOf('Preview', module)
+  .add('pdf-button', () => (
+    <div style={{ margin: '1rem' }}>
+      <PdfButton pdf="1.pdf" name="name.pdf" />
+      <hr />
+      <PdfButton pdf="2.pdf" />
+      <hr />
+      <PdfButton pdf="1.pdf" name="name.pdf" title="name.pdf" />
+    </div>
+  ))
+  .add('preview', () => (
+    <div style={{ margin: '1rem' }}>
+      <Preview text={JSON.stringify({ a: 1, b: new Date(), c: '123' })} jsonMode />
+      <hr />
+      <Preview text={JSON.stringify({ a: 1, b: new Date(), c: '234' })} listMode />
+    </div>
+  ));
