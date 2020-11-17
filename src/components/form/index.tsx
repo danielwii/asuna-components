2b44b5b36b1a8e1c581363386031496091f959fb
mercurywang <mercwang@hotmail.com>
2020-11-17 21:45:15 +0800

    refactor: form

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -50,197 +50,202 @@ export function RenderInputComponent<Values, InputValue>({
 }) {
   // useLogger('RenderInputComponent', { fieldDef, field, value });
 
+  const label = field.name === fieldDef.name ? field.name : `${field.name} / ${fieldDef.name}`;
+  const name = field.name;
+
+  const TheFileds = {
+    boolean: (
+      <React.Fragment>
+        <label>{fieldDef.name}</label>
+        <Switch
+          onChange={(checked: boolean, event: MouseEvent) =>
+            field.onChange({ target: { id: field.name, name: field.name, value: checked } })
+          }
+          defaultChecked={value}
+        />
+      </React.Fragment>
+    ),
+    color: (
+      <React.Fragment>
+        <label>{field.name}</label>
+        <SketchPicker
+          css={css`
+            margin: 1rem;
+          `}
+          color={value}
+          onChange={(color) => {
+            changeAntdTheme(generateThemeColor(color.hex));
+            field.onChange({ target: { id: field.name, name: field.name, value: color } });
+          }}
+        />
+      </React.Fragment>
+    ),
+    image: (
+      <React.Fragment>
+        <label>{field.name}</label>
+        <Uploader
+          {...{ adapter: new DefaultFileUploaderAdapterImpl(), ...fieldDef.field.extra }}
+          // adapter={fieldDef.field.extra?.adapter ?? new DefaultFileUploaderAdapterImpl()}
+          value={value}
+          onChange={(newValue) => {
+            field.onChange({ target: { id: field.name, name: field.name, value: newValue } });
+          }}
+        />
+      </React.Fragment>
+    ),
+    string: (
+      <React.Fragment>
+        <label>{label}</label>
+        <Input id={field.name} {...field} value={value} />
+      </React.Fragment>
+    ),
+    json: (
+      <React.Fragment>
+        <label>{label}</label>
+        <Input.TextArea id={field.name} {...field} autoSize rows={4} value={value} />
+      </React.Fragment>
+    ),
+    stringArray: (
+      <React.Fragment>
+        <label>{label}</label>
+        <StringArray onChange={(value) => field.onChange({ target: { id: name, name, value } })} items={value} />
+      </React.Fragment>
+    ),
+    stringTmpl: (
+      <React.Fragment>
+        <label>{label}</label>
+        <StringTmpl tmpl={value} {...field} fields={[]} {...fieldDef.field.extra} />
+      </React.Fragment>
+    ),
+    emailTmplData: (
+      <React.Fragment>
+        <label>{label}</label>
+        <DynamicJsonArrayTable
+          adapter={ObjectJsonTableHelper}
+          value={value}
+          preview={(item) => <div>{util.inspect(ObjectJsonTableHelper.keyParser(item))}</div>}
+          render={({ fieldOpts, index }) => (
+            <Card>
+              <Input {...fieldOpts('key', index)} placeholder="key" />
+              <Input {...fieldOpts('subject', index)} placeholder="subject" />
+              {/* <TextField {...fieldOpts('template', index)} label="template" /> */}
+              <WithVariable variable={fieldOpts('template', index)}>
+                {({ name, value, onChange }) => (
+                  <StringTmpl
+                    tmpl={value}
+                    fields={[]}
+                    onChange={(value) => onChange({ target: { id: name, name, value } } as any)}
+                    htmlMode
+                  />
+                )}
+              </WithVariable>
+              <pre>{JSON.stringify(field, null, 2)}</pre>
+            </Card>
+          )}
+          onChange={(values) => form.setFieldValue(field.name, values)}
+        />
+        {/* <DebugInfo data={value} type="util" /> */}
+      </React.Fragment>
+    ),
+    wxTmplData: (
+      <React.Fragment>
+        <label>{label}</label>
+        <DynamicJsonArrayTable
+          adapter={ObjectJsonTableHelper}
+          value={value}
+          preview={(item) => <div>{util.inspect(ObjectJsonTableHelper.keyParser(item))}</div>}
+          render={({ fieldOpts, index }) => (
+            <Card>
+              <Input {...fieldOpts('key', index)} placeholder="key" />{' '}
+              <Input {...fieldOpts('color', index)} placeholder="color" />
+              <Input.TextArea {...fieldOpts('value', index)} placeholder="value" autoSize />
+            </Card>
+          )}
+          onChange={(values) => form.setFieldValue(field.name, values)}
+        />
+        {/* <DebugInfo data={value} type="util" /> */}
+      </React.Fragment>
+    ),
+    wxSubscribeData: (
+      <React.Fragment>
+        <label>{label}</label>
+        <DynamicJsonArrayTable
+          adapter={ObjectJsonTableHelper}
+          value={value}
+          preview={(item) => <div>{util.inspect(ObjectJsonTableHelper.keyParser(item))}</div>}
+          render={({ fieldOpts, index }) => (
+            <Card>
+              <Input {...fieldOpts('key', index)} placeholder="key" />
+              <Input.TextArea {...fieldOpts('value', index)} placeholder="value" autoSize />
+            </Card>
+          )}
+          onChange={(values) => {
+            console.log('onChange', field.name, values);
+            form.setFieldValue(field.name, values);
+          }}
+        />
+        <DebugInfo data={value} type="util" />
+      </React.Fragment>
+    ),
+    select: (
+      <React.Fragment>
+        <label>{label}</label>
+        <AsunaSelect
+          style={{ width: 240 }}
+          placeholder={label}
+          {...field}
+          onChange={(value) => field.onChange({ target: { id: name, name, value } })}
+          value={value}
+          {...fieldDef.field.extra}
+        />
+      </React.Fragment>
+    ),
+    default: (
+      <React.Fragment>
+        <label>{label}</label>
+        <Input id={field.name} type={fieldDef.field.type} {...field} value={value} placeholder={label} />
+        {/* <DebugInfo data={{ field, fieldDef, value }} /> */}
+      </React.Fragment>
+    ),
+  };
+
   switch (fieldDef.field.type) {
     case FormFieldType.boolean: {
-      return (
-        <React.Fragment>
-          <label>{fieldDef.name}</label>
-          <Switch
-            onChange={(checked: boolean, event: MouseEvent) =>
-              field.onChange({ target: { id: field.name, name: field.name, value: checked } })
-            }
-            defaultChecked={value}
-          />
-        </React.Fragment>
-      );
+      return TheFileds.boolean;
     }
     case FormFieldType.color: {
-      return (
-        <React.Fragment>
-          <label>{field.name}</label>
-          <SketchPicker
-            css={css`
-              margin: 1rem;
-            `}
-            color={value}
-            onChange={(color) => {
-              changeAntdTheme(generateThemeColor(color.hex));
-              field.onChange({ target: { id: field.name, name: field.name, value: color } });
-            }}
-          />
-        </React.Fragment>
-      );
+      return TheFileds.color;
     }
     case FormFieldType.image: {
-      return (
-        <React.Fragment>
-          <label>{field.name}</label>
-          <Uploader
-            {...{ adapter: new DefaultFileUploaderAdapterImpl(), ...fieldDef.field.extra }}
-            // adapter={fieldDef.field.extra?.adapter ?? new DefaultFileUploaderAdapterImpl()}
-            value={value}
-            onChange={(newValue) => {
-              field.onChange({ target: { id: field.name, name: field.name, value: newValue } });
-            }}
-          />
-        </React.Fragment>
-      );
+      return TheFileds.image;
     }
     case FormFieldType.string: {
-      const label = field.name === fieldDef.name ? field.name : `${field.name} / ${fieldDef.name}`;
-      return (
-        <React.Fragment>
-          <label>{label}</label>
-          <Input id={field.name} {...field} value={value} />
-          {/* <Input id={field.name} multiline {...field} value={value} /> */}
-        </React.Fragment>
-      );
+      return TheFileds.string;
     }
     case FormFieldType.json:
     case FormFieldType.text: {
-      const label = field.name === fieldDef.name ? field.name : `${field.name} / ${fieldDef.name}`;
-      return (
-        <React.Fragment>
-          <label>{label}</label>
-          <Input.TextArea id={field.name} {...field} autoSize rows={4} value={value} />
-          {/* <Input id={field.name} multiline {...field} value={value} /> */}
-        </React.Fragment>
-      );
+      return TheFileds.json;
     }
-    case 'select': {
-      const label = field.name === fieldDef.name ? field.name : `${field.name} / ${fieldDef.name}`;
-      const name = field.name;
-      return (
-        <React.Fragment>
-          <label>{label}</label>
-          <AsunaSelect
-            style={{ width: 240 }}
-            placeholder={label}
-            {...field}
-            onChange={(value) => field.onChange({ target: { id: name, name, value } })}
-            value={value}
-            {...fieldDef.field.extra}
-          />
-        </React.Fragment>
-      );
-    }
-    case 'stringArray': {
-      const label = field.name === fieldDef.name ? field.name : `${field.name} / ${fieldDef.name}`;
-      const name = field.name;
-      return (
-        <React.Fragment>
-          <label>{label}</label>
-          <StringArray onChange={(value) => field.onChange({ target: { id: name, name, value } })} items={value} />
-        </React.Fragment>
-      );
+    case FormFieldType.stringArray: {
+      return TheFileds.stringArray;
     }
     case FormFieldType.stringTmpl: {
-      const label = field.name === fieldDef.name ? field.name : `${field.name} / ${fieldDef.name}`;
-      return (
-        <React.Fragment>
-          <label>{label}</label>
-          <StringTmpl tmpl={value} {...field} fields={[]} {...fieldDef.field.extra} />
-        </React.Fragment>
-      );
+      return TheFileds.stringTmpl;
     }
     case FormFieldType.emailTmplData: {
-      const label = field.name === fieldDef.name ? field.name : `${field.name} / ${fieldDef.name}`;
-      return (
-        <React.Fragment>
-          <label>{label}</label>
-          <DynamicJsonArrayTable
-            adapter={ObjectJsonTableHelper}
-            value={value}
-            preview={(item) => <div>{util.inspect(ObjectJsonTableHelper.keyParser(item))}</div>}
-            render={({ fieldOpts, index }) => (
-              <Card>
-                <Input {...fieldOpts('key', index)} placeholder="key" />
-                <Input {...fieldOpts('subject', index)} placeholder="subject" />
-                {/* <TextField {...fieldOpts('template', index)} label="template" /> */}
-                <WithVariable variable={fieldOpts('template', index)}>
-                  {({ name, value, onChange }) => (
-                    <StringTmpl
-                      tmpl={value}
-                      fields={[]}
-                      onChange={(value) => onChange({ target: { id: name, name, value } } as any)}
-                      htmlMode
-                    />
-                  )}
-                </WithVariable>
-                <pre>{JSON.stringify(field, null, 2)}</pre>
-              </Card>
-            )}
-            onChange={(values) => form.setFieldValue(field.name, values)}
-          />
-          {/* <DebugInfo data={value} type="util" /> */}
-        </React.Fragment>
-      );
+      return TheFileds.emailTmplData;
     }
     case FormFieldType.wxTmplData: {
-      const label = field.name === fieldDef.name ? field.name : `${field.name} / ${fieldDef.name}`;
-      return (
-        <React.Fragment>
-          <label>{label}</label>
-          <DynamicJsonArrayTable
-            adapter={ObjectJsonTableHelper}
-            value={value}
-            preview={(item) => <div>{util.inspect(ObjectJsonTableHelper.keyParser(item))}</div>}
-            render={({ fieldOpts, index }) => (
-              <Card>
-                <Input {...fieldOpts('key', index)} placeholder="key" />{' '}
-                <Input {...fieldOpts('color', index)} placeholder="color" />
-                <Input.TextArea {...fieldOpts('value', index)} placeholder="value" autoSize />
-              </Card>
-            )}
-            onChange={(values) => form.setFieldValue(field.name, values)}
-          />
-          {/* <DebugInfo data={value} type="util" /> */}
-        </React.Fragment>
-      );
+      return TheFileds.wxTmplData;
     }
     case FormFieldType.wxSubscribeData: {
-      const label = field.name === fieldDef.name ? field.name : `${field.name} / ${fieldDef.name}`;
-      return (
-        <React.Fragment>
-          <label>{label}</label>
-          <DynamicJsonArrayTable
-            adapter={ObjectJsonTableHelper}
-            value={value}
-            preview={(item) => <div>{util.inspect(ObjectJsonTableHelper.keyParser(item))}</div>}
-            render={({ fieldOpts, index }) => (
-              <Card>
-                <Input {...fieldOpts('key', index)} placeholder="key" />
-                <Input.TextArea {...fieldOpts('value', index)} placeholder="value" autoSize />
-              </Card>
-            )}
-            onChange={(values) => {
-              console.log('onChange', field.name, values);
-              form.setFieldValue(field.name, values);
-            }}
-          />
-          <DebugInfo data={value} type="util" />
-        </React.Fragment>
-      );
+      return TheFileds.wxSubscribeData;
+    }
+    case FormFieldType.select: {
+      return TheFileds.select;
     }
     default: {
-      const label = field.name === fieldDef.name ? field.name : `${field.name} / ${fieldDef.name}`;
-      return (
-        <React.Fragment>
-          <label>{label}</label>
-          <Input id={field.name} type={fieldDef.field.type} {...field} value={value} placeholder={label} />
-          {/* <DebugInfo data={{ field, fieldDef, value }} /> */}
-        </React.Fragment>
-      );
+      return TheFileds.default;
     }
   }
 }
